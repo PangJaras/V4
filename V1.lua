9c3de95b35e2ae992ac831275382346bf935d7c9
Jaras <daermlo346@gmail.com>
2025-05-08 04:53:25 +0700

    Create V1.lua

---
+++ V1.lua
@@ -0,0 +1,24 @@
+_G.Team = "Marine" -- Marine / Pirate
+_G.Settings_V4 = {
+    ["Lever"] = true, -- ออโต้ดึงคันโยก / Auto Pull Lever
+    ["InVIPServ"] = true, -- รีเซิฟให้ฟูลมูนรี / Crash when full moon end
+    ["HelperNameList"] = { -- ตัวช่วยได้ 2 ตัว / Put the helper account name
+        "MarkXouQeN",
+        "RupertFBzrKV"
+    },
+    ["V4FarmList"] = { -- ตัวฟาร์มใส่ได้ 10 ตัวเผ่าไม่ซ้ำตัวช่วยพอ / Put account name u want to farm v4
+        "Valkyros_XHBZPW",
+        "Wraithen_EP6LH3",
+        "SumiHana_PHHS7E",
+        "Hayato_NHQIOG",
+        "XirvanoV8PKYX",
+        "Uyalvoxa_GI0IIJ",
+        "Zymvero06TP5L",
+        "YoruSakura_ZNA2JN",
+        "MisoraUBTUZX",
+        "RikuTsubasaAR4S9Z"
+    }
+}
+getgenv().Key = "MARU_V4-UTZMWMRXBGINHCI"
+getgenv().id = "760440516156063766"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MasterPClient/main/Loader.lua"))()
