36f746b18fa9fb4d1111680043b58cc39453dfa3
Jaras <daermlo346@gmail.com>
2025-05-09 04:39:01 +0700

    Update V1.lua

--- V1.lua
+++ V1.lua
@@ -3,20 +3,20 @@ _G.Settings_V4 = {
     ["Lever"] = true, -- ออโต้ดึงคันโยก / Auto Pull Lever
     ["InVIPServ"] = true, -- รีเซิฟให้ฟูลมูนรี / Crash when full moon end
     ["HelperNameList"] = { -- ตัวช่วยได้ 2 ตัว / Put the helper account name
-        "MarkXouQeN",
-        "RupertFBzrKV"
+        "Valkyros_XHBZPW",
+        "MarkXouQeN"
     },
     ["V4FarmList"] = { -- ตัวฟาร์มใส่ได้ 10 ตัวเผ่าไม่ซ้ำตัวช่วยพอ / Put account name u want to farm v4
-        "Valkyros_XHBZPW",
-        "Wraithen_EP6LH3",
-        "SumiHana_PHHS7E",
-        "Hayato_NHQIOG",
-        "XirvanoV8PKYX",
-        "Uyalvoxa_GI0IIJ",
-        "Zymvero06TP5L",
-        "YoruSakura_ZNA2JN",
-        "MisoraUBTUZX",
-        "RikuTsubasaAR4S9Z"
+        "Jinraith6YSLLB",
+        "Apinaxo_YBI5TM",
+        "SolarithEYFJI1",
+        "KyoKitsuneJ5XJ0U",
+        "Xulviro_OY2WWE",
+        "Oblivior_RKIMS2",
+        "JexarionIJQW3D",
+        "XanvexorPPKASE",
+        "Ryuugetsu_RQ5XHN",
+        "MaikaNWEXE5"
     }
 }
 getgenv().Key = "MARU_V4-UTZMWMRXBGINHCI"
